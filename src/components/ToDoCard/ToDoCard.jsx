945fea3d935d796e02af919230705aa1a8bbb008
Stefan <stefan.chen@mail.utoronto.ca>
2024-11-07 14:11:18 -0500

    create backend api call and adjust todocard component

--- src/components/ToDoCard/ToDoCard.jsx
+++ src/components/ToDoCard/ToDoCard.jsx
@@ -1,35 +1,43 @@
 import React, { useState } from 'react';
+import { deleteTask } from '../../api/taskApi';
 
-function ToDoCard({ text, onDelete }) {
+function ToDoCard({ text, id, onTaskDeleted }) {
     const [isDone, setIsDone] = useState(false);
+  
+    const handleDelete = async () => {
+      try {
+        await deleteTask(id);
+        onTaskDeleted(id);  
+      } catch (error) {
+        console.error('Failed to delete task:', error);
+      }
+    };
+  
     return (
-        <div className="todo-card">
-            <h3 className={`todo-title ${isDone ? 'done' : ''}`}>
-                {text}
-            </h3>
-            <div className="circle-container">
-                <div className="circle-1" />
-                <div className="circle-2" />
-            </div>
-
-            {/* category */}
-
-            <div
-                onClick={() => setIsDone(!isDone)}
-                className="status-toggle"
-            >
-                {isDone ? '✓ Done' : '◯ Todo'}
-            </div>
-            <button
-                onClick={onDelete}
-                className="delete-button"
-            >
-                Delete
-            </button>
-
-
+      <div className="todo-card">
+        <h3 className={`todo-title ${isDone ? 'done' : ''}`}>
+          {text}
+        </h3>
+        <div className="circle-container">
+          <div className="circle-1" />
+          <div className="circle-2" />
         </div>
+  
+        <div
+          onClick={() => setIsDone(!isDone)}
+          className="status-toggle"
+        >
+          {isDone ? '✓ Done' : '◯ Todo'}
+        </div>
+        <button
+          onClick={handleDelete}
+          className="delete-button"
+        >
+          Delete
+        </button>
+      </div>
     );
-}
+  }
+  
 
 export default ToDoCard
