2efffe29f29b0f2b839b1edee59dceb9c8ce9bce
Emily Schur <ejschur@gmail.com>
2024-11-07 13:08:31 -0800

    Change tab name and icon

--- src/components/ToDoList/ToDoList.jsx
+++ src/components/ToDoList/ToDoList.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { fetchTasks, addTask } from "../../api/taskApi";
 import ToDoCard from "../ToDoCard/ToDoCard";
 
